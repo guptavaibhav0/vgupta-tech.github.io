c4b1139643a91a606e8ee7fa40d340bfd6204487
Vaibhav Gupta <vgupta-tech@gmail.com>
2019-08-21 09:08:58 +0530

    Added moustache template logic
    Moustache template with YAML database added
    Python based page builder

---
+++ builder/main.py
@@ -0,0 +1,19 @@
+import os
+
+import yaml
+import pystache
+
+from loader import Loader
+
+dirpath = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(dirpath, "yaml_files", "main.yaml"), "r") as f:
+    data = yaml.load(f, Loader)
+
+with open(os.path.join(dirpath, "template.mustache"), "r") as f:
+    parsed_template = pystache.parse(f.read())
+
+html = pystache.render(parsed_template, data)
+
+with open("index.html", "w") as f:
+    f.write(html)

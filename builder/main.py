e559135a7abb4fa186cc988236c122da91f61bec
Vaibhav Gupta <guptavaibhav0@gmail.com>
2021-09-07 16:54:47 +0200

    update website to new design

--- builder/main.py
+++ builder/main.py
@@ -10,7 +10,7 @@ dirpath = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(dirpath, "yaml_files", "main.yaml"), "r") as f:
     data = yaml.load(f, Loader)
 
-with open(os.path.join(dirpath, "template.mustache"), "r") as f:
+with open(os.path.join(dirpath, "template.html"), "r") as f:
     parsed_template = pystache.parse(f.read())
 
 html = pystache.render(parsed_template, data)
